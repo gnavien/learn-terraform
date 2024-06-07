e3becb2e121ae2303d3479f0a5121b969306e5a5
jayasathyag <jayasathyag@gmail.com>
2024-06-07 18:48:51 +0530

    update

--- 04-data-sources/data.tf
+++ 04-data-sources/data.tf
@@ -17,3 +17,10 @@ output "price" {
   value =  data.aws_ec2_spot_price.example.spot_price
 }
 
+data "aws_security_group" "selected" {
+  name = "allow-all"
+}
+
+output "sgid" {
+  value = data.aws_security_group.selected.id
+}
