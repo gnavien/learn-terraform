67af2253a3c1d0482ca6f26bbade533fe7b92945
jayasathyag <jayasathyag@gmail.com>
2024-06-07 09:22:38 +0530

    update

---
+++ 02-outputs/output.tf
@@ -0,0 +1,16 @@
+output "sample" {
+  value = "Hello, world!"
+}
+
+# Any string in terraform need to be quoted with double quotes
+# Single quotes are not allowed
+
+output "sample1" {
+  value = "1000"
+}
+
+
+
+output "sample2" {
+  value = 1000
+}
