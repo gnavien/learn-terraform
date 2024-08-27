3f4d2be528c918b1282d744b4a59934c0fb89fca
jayasathyag <jayasathyag@gmail.com>
2024-08-27 10:29:04 +0530

    update

--- 07-module-1/main.tf
+++ 07-module-1/main.tf
@@ -8,3 +8,11 @@ module "ec2" {
   name = "sample2"
 }
 
+#
+output "public_ip_sample1" {
+  value = module.sample1.publeic_ip
+}
+
+output "public_ip_sample2" {
+  value = module.sample2.publeic_ip
+}
