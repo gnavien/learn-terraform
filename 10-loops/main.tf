ecd7180ed46b6c0c877a0945c3f57373ce800199
jayasathyag <jayasathyag@gmail.com>
2024-08-28 10:44:32 +0530

    update

--- 10-loops/main.tf
+++ 10-loops/main.tf
@@ -2,17 +2,27 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+#resource "aws_instance" "web" {
+#  ami           = data.aws_ami.example.id
+#  # count=3  # we will have 3 instances created
+#  count = length(var.instances)
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "HelloWorld"
+#  }
+#}
+
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.example.id
-  # count=3  # we will have 3 instances created
   count = length(var.instances)
+
+  ami           = data.aws_ami.example.id
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld"
+    name = var.instances[count.index]
   }
 }
-
 data "aws_ami" "example" {
   owners           = ["973714476881"]
   most_recent      = true
